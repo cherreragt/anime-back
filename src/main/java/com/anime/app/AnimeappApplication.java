d852e41b0c20a5f23c51c8e01810a5dcfde59eea
cherrera45 <cherrera@is4tech.com>
2022-07-10 11:14:27 -0600

    first commit

---
+++ src/main/java/com/anime/app/AnimeappApplication.java
@@ -0,0 +1,13 @@
+package com.anime.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AnimeappApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/anime/app/AnimeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anime.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
